b39f1e6fa075d53037b5f9f763aa061a5ace8371
sudeep prajapati <sudeeppprajapati@gmail.com>
2024-12-22 17:43:17 +0530

    Connected Front & Back,

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
 import express from "express"
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from "cors"
+
+
 
 const app = express();
 const port = process.env.PORT
 
-app.get('/test', (req, res) =>{
+app.use(express.json())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({name, email, password})
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
 
-
-// import cors from "cors"
-// import cookieParser from "cookie-parser"
-
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true
-// }))
 
