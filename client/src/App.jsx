732dbb3a7eb564232f00dece653fcfa21911165c
sudeep prajapati <sudeeppprajapati@gmail.com>
2024-12-22 11:13:10 +0530

    Add Layout and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,17 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
 
 function App() {
   return (
-    <div>
-      <header className='p-4 flex justify-between'>
-        <a href="" className='flex items-center gap-1'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 -rotate-90">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-          </svg>
-          <span className='font-bold text-xl'>airbnb</span>
-        </a>
-        <div className='flex gap-2 items-center border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
-          <div>Anywhere</div>
-          <div className="border border-l border-gray-300"></div>
-          <div>Any week</div>
-          <div className="border-l border-gray-300"></div>
-          <div>Any guests</div>
-          <button className='bg-primary text-white p-1.5 rounded-full'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-          </button>
-        </div>
-        <div className='flex gap-2 items-center border border-gray-300 rounded-full py-2 px-4 '>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <div className='overflow-hidden'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-          </div>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+      Index Page here
+    </div>
+  )
+}
+
+export default IndexPage
