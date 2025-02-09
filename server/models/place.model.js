f18a467a810ec2e2a5c56cdaa41919a4bf5181b5
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-02-09 18:53:54 +0530

    Enhance Place model by adding validation

--- server/models/place.model.js
+++ server/models/place.model.js
@@ -1,19 +1,39 @@
 import mongoose from 'mongoose';
 
+// const placeSchema = new mongoose.Schema({
+//     owner: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     },
+//     title: String,
+//     address: String,
+//     photos: [String],
+//     description: String,
+//     perks: [String],
+//     extraInfo: String,
+//     checkIn: Date,
+//     checkOut: Date,
+//     maxGuests: Number,
+//     price: Number,
+// }, { timestamps: true });
+
+
 const placeSchema = new mongoose.Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User ',
+        required: true
     },
-    title: String,
-    address: String,
+    title: { type: String, required: true },
+    address: { type: String, required: true },
     photos: [String],
-    description: String,
+    description: { type: String, required: true },
     perks: [String],
     extraInfo: String,
-    checkIn: Number,
-    checkOut: Number,
-    maxGuests: Number,
+    checkIn: { type: Date, required: true },
+    checkOut: { type: Date, required: true },
+    maxGuests: { type: Number, required: true, min: 1 },
+    price: { type: Number, required: true, min: 0 },
 }, { timestamps: true });
 
 export const Place = mongoose.model('Place', placeSchema);
