308622d1ac63f111b48b610bb391611738dde8e6
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-02-19 17:33:07 +0530

    Refactor Place model to move owner field

--- server/models/place.model.js
+++ server/models/place.model.js
@@ -1,21 +1,20 @@
 import mongoose from 'mongoose';
 
 const placeSchema = new mongoose.Schema({
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User ',
-        required: true
-    },
     title: { type: String, required: true },
     address: { type: String, required: true },
     photos: [String],
     description: { type: String, required: true },
     perks: [String],
-    extraInfo: String,
     checkIn: { type: Date, required: true },
     checkOut: { type: Date, required: true },
-    maxGuests: { type: Number, required: true, min: 1 },
-    price: { type: Number, required: true, min: 0 },
+    maxGuests: { type: Number, required: true },
+    price: { type: Number, required: true },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 }, { timestamps: true });
 
 export const Place = mongoose.model('Place', placeSchema);
