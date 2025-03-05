a89dea6abb719b425df6fd006b8e62b7874d549a
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-03-05 19:54:48 +0530

    Add password visibility toggle to Login and Register pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,6 +8,7 @@ function LoginPage() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const { setUser } = useContext(UserContext)
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
@@ -19,7 +20,7 @@ function LoginPage() {
       setUser(data.user)
 
       if (data && data.message === 'Login successful') {
-        navigate('/'); // Redirect to home page on successful login
+        navigate('/'); 
       } else {
         setError(`Login failed with status code ${response.status}`);
       }
@@ -31,8 +32,13 @@ function LoginPage() {
       }
     }
   }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className='mt-28 grow flex  justify-around items-center'>
+    <div className='mt-28 grow flex justify-around items-center'>
       <div className='mb-32'>
         <h1 className='text-4xl text-center mb-4'>Login</h1>
         <form className='max-w-md mx-auto' onSubmit={handleLogin}>
@@ -43,12 +49,32 @@ function LoginPage() {
             onChange={e => setLoginvalue(e.target.value)}
             className="block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
           />
-          <input type="password"
-            placeholder='password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            className="block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder='Password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              className="block w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
+            />
+            <span
+              onClick={togglePasswordVisibility}
+              className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+            >
+              {showPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
+              )}
+            </span>
+          </div>
           <button className='primary'>Login</button>
           {error && <div className='text-center py-2 text-red-500'>{error}</div>}
           <div className='text-center py-2 text-gray-700'>Don't have an account yet? <Link className='underline text-black' to={'/register'}>  Register now</Link>
